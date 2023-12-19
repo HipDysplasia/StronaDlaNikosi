7720291fb978ede669bb01cb547bcb91abb3e713
HipDysplasia <154024216+HipDysplasia@users.noreply.github.com>
2023-12-19 18:59:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+function option1() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "breakingbad.jpg"
+    document.getElementById('BlarkSon1');
+}
+    
+function option2() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "bladerunner.jpg"
+    document.getElementById('BlarkSon2');
+}
+    
+function option3() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "fightclub.jpg"
+    document.getElementById('BlarkSon3');
+}
+    
+function option4() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "snatch.jpg"
+    document.getElementById('BlarkSon4');
+}
+    
+function option5() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "taxidriver.jpg"
+    document.getElementById('BlarkSon5');
+}
+    
+function option6() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "national.jpg"
+    document.getElementById('BlarkSon6');
+}
+    
+function option7() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "grimsby.jpg"
+    document.getElementById('BlarkSon7');
+}
+    
+function option8() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "borat.jpg"
+    document.getElementById('BlarkSon8');
+}
+
+function option9() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "drive.jpg"
+    document.getElementById('BlarkSon8');
+}
+
+function option10() {
+    let image = document.getElementById("BlarkSon0");
+    BlarkSon0.src = "babydriver.jpg"
+    document.getElementById('BlarkSon8');
+}
+
+function changeText(element) {
+    element.textContent = '♥ kocham cię ♥';
+  }
+
+  function restoreText(element) {
+    element.textContent = 'NAJEDŹ NA MNIE';
+}
